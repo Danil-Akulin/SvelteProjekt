53b96aac37614d25d60c9e9ef7651a843248e129
opilane <danilakulin23@gmail.com>
2023-09-29 13:26:42 +0300

    WIP

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
 import * as sapper from '@sapper/server';
+const { json } = require('body-parser')
 
+const FileStore = sessionFileStore(session);
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
-const { json } = require('body-parser')
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
@@ -14,6 +17,28 @@ polka() // You can also use Express
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
+
+	.use(session({
+		secret: 'conduit',
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: 31536000
+		},
+		store: new FileStore({
+			path: `.sessions`
+		})
+	}))
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware({
+			session: req => ({
+				user: req.session && req.session.user
+			})
+		})
+	)
+
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
